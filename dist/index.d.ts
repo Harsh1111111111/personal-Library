8a006a9607848097d00b1b6102471ceba356f862
Harsh Gupta <hg60543@gmail.com>
2025-08-28 18:56:47 +0530

    removed the dist folder

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { JwtPayload } from 'jsonwebtoken';
-export declare const jwt_secret = "mysecret";
-export interface usercontent extends JwtPayload {
-    userId?: string;
-}
-//# sourceMappingURL=index.d.ts.map
